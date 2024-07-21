1d49d51b29f45a704a917e333505861a07fcb854
leeeunjin <enzn@hufs.ac.kr>
2024-07-21 16:50:27 +0900

    FEAT: 기록보기 추가

--- farmlog/tests.py
+++ farmlog/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.contrib.auth import get_user_model
+from .models import FarmLog
 
 User = get_user_model()
 
@@ -12,6 +13,7 @@ class FarmLogTests(TestCase):
         self.register_url = reverse('register')
         self.login_url = reverse('login')
         self.logs_url = reverse('farmlog-list-create')
+        self.logs_view_url = reverse('farmlog-list-view')
         self.user_data = {
             'userID': 'testuser',
             'name': 'Test User',
@@ -23,17 +25,24 @@ class FarmLogTests(TestCase):
             'password': 'testpassword123'
         }, format='json')
         self.access_token = response.data['access']
-
-    def test_create_log(self):
         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
-        response = self.client.post(self.logs_url, {
+        self.farmlog_data = {
             'date': '2024-07-21',
             'lunar_date': '2024-07-15',
             'max_temp': 30.5,
             'min_temp': 20.1,
             'weather': 'Sunny',
-            'content': 'Farm work today included planting new seeds.'
-        }, format='json')
-        print("Response status code:", response.status_code)
-        print("Response data:", response.data)  # 디버깅을 위해 응답 데이터를 출력합니다.
+            'content': 'Farm work today included planting new seeds.',
+            'user': self.user.id,
+        }
+        self.client.post(self.logs_url, self.farmlog_data, format='json')
+
+    def test_create_log(self):
+        response = self.client.post(self.logs_url, self.farmlog_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_view_logs(self):
+        response = self.client.get(self.logs_view_url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['content'], self.farmlog_data['content'])
--- farmlog/views.py
+++ farmlog/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from .models import FarmLog
 from .serializers import FarmLogSerializer
 
+# 기록하기
 class FarmLogListCreateView(generics.ListCreateAPIView):
     queryset = FarmLog.objects.all()
     serializer_class = FarmLogSerializer
@@ -10,3 +11,9 @@ class FarmLogListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+        
+# 기록보기
+class FarmLogListView(generics.ListAPIView):
+    queryset = FarmLog.objects.all()
+    serializer_class = FarmLogSerializer
+    permission_classes = [IsAuthenticated]
