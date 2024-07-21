b385f805af30ce3347d93ad67aee6b6eebbf1615
leeeunjin <enzn@hufs.ac.kr>
2024-07-21 16:43:54 +0900

    FEAT: Swagger 추가

--- farmlog/urls.py
+++ farmlog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import FarmLogListCreateView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 urlpatterns = [
     path('logs/', FarmLogListCreateView.as_view(), name='farmlog-list-create'),
